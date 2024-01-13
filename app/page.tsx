e157c4a60f29fc6b3a54dc5f8b331927e665e8c9
wolfgunblood <jalajdorai1@gmail.com>
2024-01-13 15:29:54 +0530

    Initialize app

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='font-semibold'>
+        Hello World
+      </h1>
+      
+    </div>
+  )
+}
